a8754c80f2454102feab845e1b68b13864cf4694
gaithern <gaithern@gmail.com>
2024-01-08 23:33:39 -0600

    Delete pushToOpenKH.bat

--- pushToOpenKH.bat
+++
@@ -1,9 +0,0 @@
-rem pushes each file to the openKH directory, so you can work in the repo
-rem but still can test it right away
-
-cd %~dp0
-set openKH= "E:\Games\Epic\Kingdom Hearts Mods\KH2\KH2_Randomizer\OpenKH.Mod.Manager\mod\kh1\scripts\"
-
-for %%f in (*.lua) do (
-    copy "%%f" %openKH%%%f
-)
