5b889e045324c2d6f1cfc9aae83d76cd3b9dabd7
gaithern <gaithern@gmail.com>
2024-03-25 15:36:34 -0500

    Update 1fmAPQualityOfLife.lua

--- 1fmAPQualityOfLife.lua
+++ 1fmAPQualityOfLife.lua
@@ -23,24 +23,40 @@ end
     
 function _OnFrame()
     if canExecute then
-        if ReadByte(Now+0x00) == 0x04 and ReadByte(EventFlags+0x0F06) == 0x00 then
-            WriteByte(EventFlags+0x0F06, 0xFF) --All Evidence Boxes Opened
-        end
-        if ReadByte(Items+0xDE) == 0x01 and ReadByte(EventFlags+0x00) == 0x00 then
-            WriteByte(EventFlags+0x00, 0x01) --Found Footprints
-        end
-        if ReadByte(CutsceneFlags+0x0B06) < 0x32 and ReadByte(EventFlags+0x1008) == 0x00 and ReadByte(EventFlags+0x0125) == 0x00 then
-            WriteByte(EventFlags+0x1008, 0x01) --Remove Olympus Coliseum Yellow Trinity before End of 1st Visit
-        elseif ReadByte(CutsceneFlags+0x0B06) >= 0x32 and ReadByte (EventFlags+0x1008) == 0x01 and ReadByte(EventFlags+0x0125) == 0x00 then
-            WriteByte(EventFlags+0x1008, 0x00) --Restore Olympus Coliseum Yellow Trinity after End of 1st Visit
-        end
-        if ReadByte(Now+0x00) == 0x05 and ReadByte(Now+0x68) == 0x09 and ReadByte(CutsceneFlags+0x0B05) == 0x53 then
-            WriteByte(CutsceneFlags+0x0B05, 0x50) --Deep Jungle Clayton Softlock Fix
-        end
-        if ReadByte(Items+0xE3) == 0x01 and ReadByte(CutsceneFlags+0x0B0C) == 0x2B and ReadByte(ChestFlags+0x00) == 0x02 then
-            WriteByte(CutsceneFlags+0x0B0C, 0x32) --HT Story Progression after finding Jack-in-the-Box
-            WriteByte(RoomFlags+0x19, 0x05) --Boneyard Room Flag
-            WriteByte(RoomFlags+0x1E, 0x03) --Research Lab Room Flag
-        end
+       if ReadByte(EventFlags+0x0E60) == 0x00 then --No Red Trinities Activated
+           WriteByte(EventFlags+0x0E60, 0x01) --Activated 1 Red Trinity because Secret Waterway Entrance
+       end
+       if ReadByte(EventFlags+0x0EB5) == 0x01 then --Stepped on All 3 Switches in Gizmo Shop
+           WriteByte(EventFlags+0x0EB5, 0x03) --No Wait Time for the 2 Postcards
+       end
+       if ReadByte(Now+0x00) == 0x04 and ReadByte(EventFlags+0x0F06) == 0x00 then
+           WriteByte(EventFlags+0x0F06, 0xFF) --All Evidence Boxes Opened
+       end
+       if ReadByte(Items+0xDE) == 0x01 and ReadByte(EventFlags+0x00) == 0x00 then
+           WriteByte(EventFlags+0x00, 0x01) --Found Footprints
+       end
+       if ReadShort(EventFlags+0x1001) == 0x0100 then --Only Shiva Belt Chest spawned
+           WriteByte(EventFlags+0x100A, 0x02)
+       end
+       if ReadByte(CutsceneFlags+0x0B06) < 0x32 and ReadByte(EventFlags+0x1008) == 0x00 and ReadByte(EventFlags+0x0125) == 0x00 then
+           WriteByte(EventFlags+0x1008, 0x01) --Remove Olympus Coliseum Yellow Trinity before End of 1st Visit
+       elseif ReadByte(CutsceneFlags+0x0B06) >= 0x32 and ReadByte (EventFlags+0x1008) == 0x01 and ReadByte(EventFlags+0x0125) == 0x00 then
+           WriteByte(EventFlags+0x1008, 0x00) --Restore Olympus Coliseum Yellow Trinity after End of 1st Visit
+       end
+       if ReadByte(Now+0x00) == 0x05 and ReadByte(Now+0x68) == 0x09 and ReadByte(CutsceneFlags+0x0B05) == 0x53 then
+           WriteByte(CutsceneFlags+0x0B05, 0x50) --Deep Jungle Clayton Softlock Fix
+           WriteInt(EventFlags+0x0F24, 0x00010101)
+       end
+       if ReadByte(Now+0x00) ~= 0x0C or ReadByte(Now+0x00) == 0x0C and ReadByte(Now+0x68) ~= 0x0A then --Not in Monstro: Chamber 6
+           if ReadByte(ChestFlags+0xCC) == 0x00 and ReadByte(EventFlags+0x0E6E) > 0x7F then --Activated White Trinity, Didn't Open Chest
+               WriteByte(EventFlags+0x0E63,ReadByte(EventFlags+0x0E63)-1) --Subtract Activated White Trinities by 1
+               BitNot(EventFlags+0x0E6E, 0x80) --Respawn White Trinity in Monstro: Chamber 6
+           end
+       end
+       if ReadByte(Items+0xE3) == 0x01 and ReadByte(CutsceneFlags+0x0B0C) == 0x2B and ReadByte(ChestFlags+0x00) == 0x02 then
+           WriteByte(CutsceneFlags+0x0B0C, 0x32) --HT Story Progression after finding Jack-in-the-Box
+           WriteByte(RoomFlags+0x19, 0x05) --Boneyard Room Flag
+           WriteByte(RoomFlags+0x1E, 0x03) --Research Lab Room Flag
+       end
     end
 end
