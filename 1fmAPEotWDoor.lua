50145189da13d0195be8e9a102b2efc02318a7ae
gaithern <gaithern@gmail.com>
2024-05-02 15:43:24 -0500

    Update 1fmAPEotWDoor.lua

--- 1fmAPEotWDoor.lua
+++ 1fmAPEotWDoor.lua
@@ -23,6 +23,11 @@ else
     end
 end
 
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
 function read_required_reports()
     if file_exists(client_communication_path .. "required_reports_door.cfg") then
         file = io.open(client_communication_path .. "required_reports_door.cfg", "r")
